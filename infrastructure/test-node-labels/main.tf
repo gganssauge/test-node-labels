e2e4b41d13056d4f2b85805f0edb19dbb9eb7622
Gottfried Ganßauge <Gottfried.Ganssauge@haufe-lexware.com>
2023-02-07 12:18:43 +0100

    - Eliminate variables.tf as well.

--- infrastructure/test-node-labels/main.tf
+++ infrastructure/test-node-labels/main.tf
@@ -84,6 +84,19 @@ output "kubeconfig" {
   sensitive   = true
 }
 
+variable "agent-count" {
+  description = "Number of agents per pool"
+  type        = number
+}
+
+variable "resource-group" {
+  description = "cluster resource group"
+  type        = object({
+    name     = string // name of the resource group
+    location = string // location of the resource group - will be used for all server resources
+  })
+}
+
 locals {
   vm_tags = {
     hg-az_lz-resource-owner              = "Aurora"
